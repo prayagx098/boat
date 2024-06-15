b3c61b312888aeb3ce36a9ae2865a7689ab00660
prayag <prayagjishnu123@gmail.com>
2024-06-16 00:53:08 +0530

    final commit

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-document.querySelector('.prev-icon').addEventListener('click', function() {
-    document.querySelector('.products-container').scrollBy({
-      left: -300,
-      behavior: 'smooth'
-    });
-  });
+// document.querySelector('.prev-icon').addEventListener('click', function() {
+//     document.querySelector('.products-container').scrollBy({
+//       left: -300,
+//       behavior: 'smooth'
+//     });
+//   });
   
-  document.querySelector('.next-icon').addEventListener('click', function() {
-    document.querySelector('.products-container').scrollBy({
-      left: 300,
-      behavior: 'smooth'
+//   document.querySelector('.next-icon').addEventListener('click', function() {
+//     document.querySelector('.products-container').scrollBy({
+//       left: 300,
+//       behavior: 'smooth'
+//     });
+//   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const boxes = document.querySelectorAll('.box');
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          console.log('hello');
+        } else {
+          entry.target.classList.remove('visible');
+        }
+      });
+    }, { threshold: 0.1 });
+  
+    boxes.forEach(box => {
+      observer.observe(box);
     });
   });
   
